ef20ee7150a37ba990b302c724464783f0b09175
brindadacha <146880465+brindadacha@users.noreply.github.com>
2023-10-04 15:03:51 -0400

    Update streamlit_app.py
    imported package pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free- Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header("🍌🥭 Build Your Own Smoothie 🥝🍇")
+import pandas
